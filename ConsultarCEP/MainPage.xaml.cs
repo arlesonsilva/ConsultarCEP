8dce789461332857b2a12887266edd10d061cae9
Arleson <arlesonsilva@hotmail.com>
2018-10-13 00:22:42 -0300

    Criacao servico modelo classe endereco e layout de tela

--- ConsultarCEP/MainPage.xaml.cs
+++ ConsultarCEP/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ConsultarCEP.Service;
+using ConsultarCEP.Service.Model;
 
 namespace ConsultarCEP
 {
@@ -12,6 +14,40 @@ namespace ConsultarCEP
         public MainPage()
         {
             InitializeComponent();
+            btBuscar.Clicked += SearchCEP;
+        }
+
+        private void SearchCEP(object sender, EventArgs args) 
+        {
+            String cep = lbCep.Text.Trim();
+            if (isValidCEP(cep)) 
+            {
+                Address address = CepService.SearchAddressCEP(cep);
+                lbResultado.Text = string.Format("Endereco: {0} \nBairro: {1} \nCidade: {2} \nEstado: {3}",
+                                                 address.logradouro,
+                                                 address.bairro,
+                                                 address.localidade,
+                                                 address.uf);
+            }
+        }
+
+        private bool isValidCEP(String cep)
+        {
+            bool erro = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO","CEP deve conter 8 caracteres!","Ok");
+                erro = false;
+            }
+
+            int cepValid = 0;
+            if (!int.TryParse(cep, out cepValid))
+            {
+                DisplayAlert("ERRO", "CEP deve conter apenas números!", "Ok");
+                erro = false;
+            }
+
+            return erro;
         }
     }
 }
---
+++ ConsultarCEP/Service/CepService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using ConsultarCEP.Service.Model;
+using Newtonsoft.Json;
+
+namespace ConsultarCEP.Service
+{
+    public class CepService
+    {
+        private static string AddressURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Address SearchAddressCEP(string cep) 
+        {
+            string NewAddressURL = string.Format(AddressURL, cep);
+
+            WebClient webClient = new WebClient();
+            String Content = webClient.DownloadString(NewAddressURL);
+
+            Address address = JsonConvert.DeserializeObject<Address>(Content);
+
+            return address;
+        }
+    }
+}
